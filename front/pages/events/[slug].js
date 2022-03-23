218897f7f0e956aeea684c1f6285914d255c0241
NikosKouroumpetsis <kourou67s@gmail.com>
2022-03-23 20:24:37 +0200

    connect front with back and search component

--- front/pages/events/[slug].js
+++ front/pages/events/[slug].js
@@ -1,68 +1,16 @@
-import React from "react";
-import Layout from "../components/Layout";
-import styles from "@/styles/Event.module.css";
-import { API_URL } from "@/config/index";
+// import { ToastContainer, toast } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css'
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import Link from "next/link";
 import Image from "next/image";
-import { FaPencilAlt, FaTimes } from "react-icons";
-
-function EventPage() {
-  const deleteEvent = (e) => {
-    console.log("delete");
-  };
-  return (
-    <Layout>
-      <div className={styles.event}>
-        <div className={styles.controls}>
-          <Link href={`/events/edit/${evt.id}`}>
-            <a>
-              <FaPencilAlt /> Edit Event
-            </a>
-          </Link>
-          <a href="#" className={styles.delete} onClick={deleteEvent}>
-            <FaTimes /> Delete Event
-          </a>
-        </div>
-        <span>
-          {evt.date} at {evt.time}
-        </span>
-        <h1>{evt.name}</h1>
-        {evt.image && (
-          <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} />
-          </div>
-        )}
-        <h3>Performers:</h3>
-        <p>{evt.performers}</p>
-        <h3>Description</h3>
-        <p>{evt.description}</p>
-        <h3>Venue: {evt.venue}</h3>
-        <p>{evt.address}</p>
-
-        <Link href="/events">
-          <a className={styles.back}>{"<"} Go Back</a>
-        </Link>
-      </div>
-    </Layout>
-  );
-}
-
-export default EventPage;
-
-// export async function getServerSideProps({ query: slug }) {
-//   const res = await fetch(`${API_URL}/api/events/${slug}`);
-
-//   const events = await res.json()
-
-//   return {
-//     props: {
-//       evt:events[0],
-//     },
-//   };
-// }
+import Layout from "@/components/Layout";
+// import EventMap from '@/components/EventMap'
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import { useRouter } from "next/router";
 
 export async function getStaticPaths() {
-  const res = await fetch(`{API_URL}/api/events`);
+  const res = await fetch(`http://127.0.0.1:1337/events`);
   const events = await res.json();
 
   const paths = events.map((evt) => ({ params: { slug: evt.slug } }));
@@ -73,8 +21,8 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params: slug }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`http://127.0.0.1:1337/events?slug=${slug}`);
 
   const events = await res.json();
 
@@ -84,4 +32,42 @@ export async function getStaticProps({ params: slug }) {
     },
     revalidate: 1,
   };
+}
+
+export default function EventPage({ evt }) {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <div className={styles.event}>
+        <span>
+          {new Date(evt.date).toLocaleDateString("gr-GR")} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {/* <ToastContainer /> */}
+        {evt.image && (
+          <div className={styles.image}>
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+            />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        {/* <EventMap evt={evt} /> */}
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
+      </div>
+    </Layout>
+  );
 }
--- front/pages/events/add.js
+++ front/pages/events/add.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "@/components/Layout";
 
 function Add() {
   return <Layout title="add Event">Add</Layout>;
